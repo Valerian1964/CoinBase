e9e6a39840421921f83834a79db23c2a9f27f267
Valeri <35935215+Valerian1964@users.noreply.github.com>
2025-09-02 18:20:57 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/coinbase/main.go
@@ -0,0 +1,23 @@
+// cmd/coinbase/main.go
+package main
+
+import (
+    "flag"
+    "log"
+    "os"
+
+    "coinbase/internal/coinbase"
+)
+
+func main() {
+    verbose := flag.Bool("verbose", false, "Enable verbose logging")
+    input := flag.String("input", "", "Input file path")
+    output := flag.String("output", "", "Output file path")
+    flag.Parse()
+
+    app := coinbase.NewApp(*verbose)
+    if err := app.Run(*input, *output); err != nil {
+        log.Printf("Error: %v", err)
+        os.Exit(1)
+    }
+}
